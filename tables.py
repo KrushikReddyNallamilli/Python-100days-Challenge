beed996d4f4f67fd528f970b892f99f57ce6c8d1
Nallamilli S V S R Krushik Reddy <52321852+KrushikReddyNallamilli@users.noreply.github.com>
2020-08-15 12:28:26 +0530

    Tables Program

---
+++ tables.py
@@ -0,0 +1,11 @@
+n=int(input("Enter a number: "))
+sr=int(input("Enter the starting range: "))
+er=int(input("Enter the ending range: "))
+
+if(sr>er):
+	for i in range(sr,er-1,-1):
+		print(n,'*',i,'=',n*i)
+else:
+	for i in range(sr,er+1):
+		print(n,'*',i,'=',n*i)
+
