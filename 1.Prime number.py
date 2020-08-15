84dfba680197be67969e67588b548fa905c3d08b
Nallamilli S V S R Krushik Reddy <52321852+KrushikReddyNallamilli@users.noreply.github.com>
2020-08-15 11:10:42 +0530

    first Commit

---
+++ 1.Prime number.py
@@ -0,0 +1,53 @@
+#prime number
+
+# count=0
+# n=int(input("Enter a number"))
+# for i in range (1,n+1):
+# 	if(n%i==0):
+# 		count+=1;
+# if count==2:
+# 	print(n,"is prime")
+# else :
+# 	print(n,"is not a prime")
+
+# count=0
+# n=int(input("Enter a number"))
+# for i in range (2,n):
+# 	if(n%i==0):
+# 		count+=1;
+# if count>0:
+# 	print(n,"is not prime")
+# else :
+# 	print(n,"is a prime")
+
+# count=0
+# n=int(input("Enter a number"))
+# if n>1:
+# 	for i in range (2,n):
+# 		if(n%i==0):
+# 			print(n,"is not prime")
+# 			break
+# 	else :
+# 		print(n,"is a prime")
+# else:
+# 	print("enter number greater than 1")
+
+# count=0
+# n=int(input("Enter a number"))
+# for i in range (2,n//2+1):
+# 	if(n%i==0):
+# 		count+=1;
+# if count>0:
+# 	print(n,"is not prime")
+# else :
+# 	print(n,"is a prime")
+
+import math
+n=int(input("Enter a number"))
+for i in range (2,abs(int(math.sqrt(n)))+1):
+	if(n%i==0):
+		print(n,"is not prime")
+		break
+else :
+	print(n,"is a prime")
+
