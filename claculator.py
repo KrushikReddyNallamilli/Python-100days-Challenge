24f68f54b222dcae6af1a4624d2a5a1bbc89250b
Nallamilli S V S R Krushik Reddy <52321852+KrushikReddyNallamilli@users.noreply.github.com>
2020-08-17 11:16:00 +0530

    Add files via upload

---
+++ claculator.py
@@ -0,0 +1,14 @@
+def add(a,b):
+	return(a+b)
+def substract(a,b):
+	return(a-b)
+def multiply(a,b):
+	return(a*b)
+def division(a,b):
+	return(a/b)
+a=int(input("Enter a number: "))
+b=int(input("Enter a number: "))
+print("Addition of given numbers is ",add(a,b))
+print("Substraction of given numbers is",substract(a,b))
+print("Multiplication of given numbers is",multiply(a,b))
+print("Division of given numbers is",division(a,b))
