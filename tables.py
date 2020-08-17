51902629f49124e6fa5f99d7259f3ac2d6f24e64
Nallamilli S V S R Krushik Reddy <52321852+KrushikReddyNallamilli@users.noreply.github.com>
2020-08-17 11:15:28 +0530

    Add files via upload

--- tables.py
+++ tables.py
@@ -4,8 +4,8 @@ er=int(input("Enter the ending range: "))
 
 if(sr>er):
 	for i in range(sr,er-1,-1):
-		print(n,'*',i,'= ',n*i)
+		print(n,'*',i,'=',n*i)
 else:
 	for i in range(sr,er+1):
-		print(n,'*',i,'= ',n*i)
+		print(n,'*',i,'=',n*i)
 
