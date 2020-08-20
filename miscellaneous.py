609446db257fed4508c78e401d357dfdeecd5672
Nallamilli S V S R Krushik Reddy <52321852+KrushikReddyNallamilli@users.noreply.github.com>
2020-08-20 11:26:10 +0530

    Add files via upload

---
+++ miscellaneous.py
@@ -0,0 +1,15 @@
+# normal way of taking input
+# a=int(input("Enter a number:"))
+# b=int(input("Enter a number:"))
+# c=int(input("Enter a number:"))
+# print(a+b+c)
+
+#taking input in the same line
+# a,b=map(int,input("Enter two numbers").split())
+# print(a+b)
+
+#swap
+a=int(input("Enter a number:"))
+b=int(input("Enter a number:"))
+a,b=b,a
+print("a=",a,"b=",b)
