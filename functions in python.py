dba0a137649fe0ab675517f848c37ab7b42ff666
Nallamilli S V S R Krushik Reddy <52321852+KrushikReddyNallamilli@users.noreply.github.com>
2020-08-20 12:56:12 +0530

    functions in python
    functions in python

---
+++ functions in python.py
@@ -0,0 +1,32 @@
+# print("hi")
+# def add():	
+# 	a=int(input("Enter a number"))
+# 	b=int(input("Enter a number"))
+# 	print(a+b)
+# print("hello")
+# add()
+# print("example")
+# add()
+
+# def add():	
+# 	a=int(input("Enter a number"))
+# 	b=int(input("Enter a number"))
+# 	return(a+b)
+# print(add())
+
+# def add(a,b):
+# 	return(a+b)
+# a=int(input("Enter a number: "))
+# b=int(input("Enter a number: "))
+# print(add(a,b))
+
+# def add(m,n):#formal parameters
+# 	return(m+n)
+# a=int(input("Enter a number: "))
+# b=int(input("Enter a number: "))
+# print(add(a,b)) #actual parameters
+
+def add(a,b):
+	return(a+b)
+print(add(a,b))
+print(add(10,20))
