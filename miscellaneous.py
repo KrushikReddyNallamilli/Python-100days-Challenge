157839e3d29c367f5ac4d3f8e2eb25f5f52ac80f
Nallamilli S V S R Krushik Reddy <52321852+KrushikReddyNallamilli@users.noreply.github.com>
2020-08-23 18:35:03 +0530

    Update miscellaneous.py

--- miscellaneous.py
+++ miscellaneous.py
@@ -9,7 +9,34 @@
 # print(a+b)
 
 #swap
-a=int(input("Enter a number:"))
-b=int(input("Enter a number:"))
-a,b=b,a
-print("a=",a,"b=",b)
+# a=int(input("Enter a number:"))
+# b=int(input("Enter a number:"))
+# a,b=b,a
+# print("a=",a,"b=",b)
+
+# import math
+
+# a=math.ceil(23.68543516)
+# b=math.ceil(-3.51603552)
+
+# c=math.floor(23.68543516)
+# d=math.floor(-3.51603552)
+
+# print(a,b,c,d)
+
+# from math import ceil,floor
+# a=ceil(23.68543516)
+# b=ceil(-3.51603552)
+# c=floor(23.68543516)
+# d=floor(-3.51603552)
+# print(a,b,c,d)
+
+
+# from math import *
+# a=ceil(23.68543516)
+# b=ceil(-3.51603552)
+# c=floor(23.68543516)
+# d=floor(-3.51603552)
+# print(a,b,c,d)
+
+
