99c9c72e930f02345210b9b8b32939c8d3b4d5af
Nallamilli S V S R Krushik Reddy <52321852+KrushikReddyNallamilli@users.noreply.github.com>
2020-09-01 16:59:48 +0530

    Add files via upload

---
+++ diamond pattern .py
@@ -0,0 +1,23 @@
+# def pattern(n):
+#       k = 2 * n - 2
+#       for i in range(0,n):
+#            for j in range(0,k):
+#                print(end=" ")
+#            k = k - 1
+#            for j in range(0, i+1):
+#                 print("*", end=" ")
+#            print(" ")
+ 
+# pattern(5)
+
+
+
+def pattern(n):
+	for i in range(n,-n,-1):
+		for j in range(1,abs(i)+1):
+			print(' ',end='')
+		for k in range(n,abs(i),-1):
+			print("* ",end="")
+		print()
+n=int(input())
+pattern(n)
