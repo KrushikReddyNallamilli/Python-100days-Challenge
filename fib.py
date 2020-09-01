ccb004cdc24fb0671972a49350067caaa7ba62dd
Nallamilli S V S R Krushik Reddy <52321852+KrushikReddyNallamilli@users.noreply.github.com>
2020-09-01 17:30:07 +0530

    Add files via upload

--- fib.py
+++ fib.py
@@ -16,21 +16,37 @@
 # n=int(input("Enter a number"))
 # fib(n)
 
+# def fib(n):
+# 	a=0
+# 	b=1
+# 	sum=1
+# 	print(a,b,end=" ")
+# 	i=2
+# 	while i<n:
+# 		c=a+b
+# 		sum+=c
+# 		print(c,end=" ")
+# 		a=b
+# 		b=c
+# 		i+=1
+# 	else:
+# 		print("\nlast element:",c)
+# 		print("sum=",sum)	
+# n=int(input("Enter a number"))
+# fib(n)
+
+
+
+
+
+#Using recurssion
+
+
 def fib(n):
-	a=0
-	b=1
-	sum=1
-	print(a,b,end=" ")
-	i=2
-	while i<n:
-		c=a+b
-		sum+=c
-		print(c,end=" ")
-		a=b
-		b=c
-		i+=1
+	if n==1:
+		return 0
+	elif n==2:
+		return 1
 	else:
-		print("\nlast element:",c)
-		print("sum=",sum)	
-n=int(input("Enter a number"))
-fib(n)
+		return(fib(n-1)+fib(n-2))
+print(fib(10))
